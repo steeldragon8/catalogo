10f5dbba40dbef5e3cbbd8eb7dd5cc1b5e9a2570
Angel Lopez <aabrahamlopez@gmail.com>
2023-11-10 16:28:45 -0600

    Layout base

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"bvp": ["Be Vietnam Pro"]
+			},
+			backgroundImage: {
+				"close-menu": "url('../assets/icon-close.svg')",
+				"open-menu": "url('../assets/icon-open.svg')"
+			}
+		},
 	},
 	plugins: [],
 }
